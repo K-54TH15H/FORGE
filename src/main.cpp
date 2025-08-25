ce07b703a02f91d2f8a8930be3bd0a742c7c1914
K-54TH15H <sathish061006@gmail.com>
2025-08-25 11:58:57 +0530

    ForgeFile Indication

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 
 int main(int argc,char* argv[])
 {
+	//Help Section
 	if(argc == 2 && (!strcmp(argv[1],"-h") || !strcmp(argv[1],"--help")))
 	{
 		std::cout << ">>> HELP SECTION <<<" << std::endl;
 		std::cout << "--- no functionality ---" << std::endl;
+	}
+	else
+	{
+		std::ifstream forgeFile("ForgeFile");
+
+		if(forgeFile.is_open())
+		{
+			std::cout << "ForgeFile found ---> Compilation to be started" << std::endl;
+		}
+		else
+		{
+			std::cout << "ForgeFile not found ---> Exiting with Error Code 00" << std::endl;
+			throw 00;
+		}
 	}
 }
